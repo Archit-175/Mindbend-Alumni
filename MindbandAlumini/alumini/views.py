d4c3e1c473f7082c03fa9d9e76000a12951933fa
bhaskar <bhscodz7@gmail.com>
2025-02-23 12:17:21 +0530

    add alumini page , create static , move images to static from faculty template,create urls for faculty and alumini

--- MindbandAlumini/alumini/views.py
+++ MindbandAlumini/alumini/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def alumini_page(request):
+    return render(request,"alumini/alumini.html")
--- MindbandAlumini/faculty/views.py
+++ MindbandAlumini/faculty/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
+def faculty_page(request):
+    return render(request,"faculty/faculty.html")
